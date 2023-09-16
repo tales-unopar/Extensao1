27e0c244c4c3d1f995a6ffb06d10a2581ec0b335
Tales <talesconrado@icloud.com>
2023-09-16 12:55:44 -0300

    Initial Commit

---
+++ PlannerApp/PlannerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlannerAppApp.swift
+//  PlannerApp
+//
+//  Created by Tales on 16/09/23.
+//
+
+import SwiftUI
+
+@main
+struct PlannerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
